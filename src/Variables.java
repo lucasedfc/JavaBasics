5abe07d9991f70a7c24c557fc0de3e8de963f3bb
Lucas Heim <lucasedfc@gmail.com>
2021-07-08 16:01:58 -0300

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Declare
+        int speed;
+        // Assign value
+        speed = 10;
+        System.out.println("Speed: " + speed);
+
+        int salary = 2500;
+        String employeeName = "Charles";
+        System.out.println("Name: " + employeeName);
+    }
+}
